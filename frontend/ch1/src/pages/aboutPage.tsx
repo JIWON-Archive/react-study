fb806417b14d4ee71f1c2461ec7c7639085ee8c1
jiwon <asdfoqlk@naver.com>
2026-02-06 19:31:05 +0900

    NavLink

--- frontend/ch1/src/pages/aboutPage.tsx
+++ frontend/ch1/src/pages/aboutPage.tsx
@@ -1,7 +1,11 @@
+import { NavLink } from "react-router";
+
 function AboutPage() {
     return (
         <div className="text-3xl">
-            <div>About Page</div>
+            <div>About Page
+                <NavLink to="/">Main</NavLink>
+            </div>
         </div>
     );
 }
--- frontend/ch1/src/pages/main-page.tsx
+++ frontend/ch1/src/pages/main-page.tsx
@@ -1,7 +1,10 @@
+import { NavLink } from "react-router";
+
 function MainPage() {
     return (
         <div className="text-3xl">
             <div>Main Page</div>
+                <NavLink to="/about">About</NavLink>
         </div>
     );
 }
