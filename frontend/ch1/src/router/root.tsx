40bf64971164cfc778605462720e6ea032e4a331
jiwon <asdfoqlk@naver.com>
2026-02-10 21:43:24 +0900

    공통레이아웃설정

--- frontend/ch1/src/router/root.tsx
+++ frontend/ch1/src/router/root.tsx
@@ -1,19 +1,42 @@
-import { lazy, Suspense } from "react";
-import { createBrowserRouter } from "react-router";
+import { lazy, Suspense } from 'react';
+import { createBrowserRouter } from 'react-router';
+import BasicLayout from '../layouts/basicLayout';
 // 페이지를 필요할 때만 로딩하기 위해 lazy와 Suspense 사용
-const Loading = () => <div>Loading...</div>
-const Main = lazy(() => import("../pages/main-page"));
-const About = lazy(() => import("../pages/aboutPage"));
+const Loading = () => <div>Loading...</div>;
+const Main = lazy(() => import('../pages/main-page'));
+const About = lazy(() => import('../pages/aboutPage'));
 
 const router = createBrowserRouter([
+  // {
+  //   path: "",
+  //   element: <Suspense fallback={<Loading />}><Main /></Suspense>,
+  // },
+  // {
+  //   path: "about",
+  //   element: <Suspense fallback={<Loading />}><About /></Suspense>,
+  // }
   {
-    path: "",
-    element: <Suspense fallback={<Loading />}><Main /></Suspense>,
+    path: '',
+    Component: BasicLayout,
+    children: [
+      {
+        index: true,
+        element: (
+          <Suspense fallback={<Loading />}>
+            <Main />
+          </Suspense>
+        ),
+      },
+      {
+        path: 'about',
+        element: (
+          <Suspense fallback={<Loading />}>
+            <About />
+          </Suspense>
+        ),
+      },
+    ],
   },
-  {
-    path: "about",
-    element: <Suspense fallback={<Loading />}><About /></Suspense>,
-  }
 ]);
 
 export default router;
