c323556152a47ff165a5fca402ed113b3da6f5d2
jiwon <asdfoqlk@naver.com>
2026-02-20 11:55:47 +0900

    주석 추가

--- frontend/ch1/src/router/root.tsx
+++ frontend/ch1/src/router/root.tsx
@@ -1,9 +1,12 @@
 import { lazy, Suspense } from 'react';
 import { createBrowserRouter } from 'react-router';
 import BasicLayout from '../layouts/basicLayout';
-// 페이지를 필요할 때만 로딩하기 위해 lazy와 Suspense 사용
+
+// 로딩 중에 보여줄 임시 컴포넌트
 // eslint-disable-next-line react-refresh/only-export-components
 const Loading = () => <div>Loading...</div>;
+// 페이지를 필요할 때만 로딩하기 위해 lazy와 Suspense 사용
+// 컴포넌트를 필요할 때만 가져오는 지연 로딩(Lazy Loading) 설정
 const Main = lazy(() => import('../pages/main-page'));
 const About = lazy(() => import('../pages/aboutPage'));
 
@@ -18,12 +21,12 @@ const router = createBrowserRouter([
   // }
   {
     path: '',
-    Component: BasicLayout,
-    children: [
+    Component: BasicLayout, // 모든 자식 페이지에 적용될 공통 레이아웃 (부모)
+    children: [             // 레이아웃 내부의 <Outlet /> 자리에 들어갈 자식들
       {
-        index: true,
+        index: true,        // index: true는 path가 빈 문자열일 때 이 요소를 렌더링하라는 의미
         element: (
-          <Suspense fallback={<Loading />}>
+          <Suspense fallback={<Loading />}>   
             <Main />
           </Suspense>
         ),
