3464b9d188b74439e2358581b082a4e5bb680d10
jiwon <asdfoqlk@naver.com>
2026-02-20 18:55:03 +0900

    useParams

--- frontend/ch1/src/router/todoRouter.tsx
+++ frontend/ch1/src/router/todoRouter.tsx
@@ -1,8 +1,10 @@
 import { lazy, Suspense } from 'react';
+
 // eslint-disable-next-line react-refresh/only-export-components
 const Loading = () => <div>Loading...</div>;
 const TodoIndex= lazy(() => import('../pages/todo/indexPage'));
 const TodoList = lazy(() => import('../pages/todo/listPage'));
+const ReadPage = lazy(() => import('../pages/todo/readPage'));
 
 const todoRouter = () => {
     return ( 
