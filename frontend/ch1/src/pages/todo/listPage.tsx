f030dddec54add2b19945e25308e774c496456b6
jiwon <asdfoqlk@naver.com>
2026-02-20 19:12:41 +0900

    useSearchParams

--- frontend/ch1/src/pages/todo/listPage.tsx
+++ frontend/ch1/src/pages/todo/listPage.tsx
@@ -1,7 +1,14 @@
+import { useSearchParams } from "react-router";
+
 function ListPage() {
+    const [queryParams] = useSearchParams();
+    const page: string | null = queryParams.get('page');
+    const size: string | null = queryParams.get('size');
+
+
     return ( 
         <div className="bg-white w-full">
-            <div>Tode List Page</div>
+            <div>Tode List Page {page} {size}</div>
         </div>
     );
 }
