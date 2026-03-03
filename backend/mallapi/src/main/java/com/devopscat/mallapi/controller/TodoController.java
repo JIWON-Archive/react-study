8686205eaa8061e23648ff002140101259a23534
jiwon <asdfoqlk@naver.com>
2026-03-04 00:28:45 +0900

    Re@stController ê CRUD

--- backend/mallapi/src/main/java/com/devopscat/mallapi/controller/TodoController.java
+++ backend/mallapi/src/main/java/com/devopscat/mallapi/controller/TodoController.java
@@ -1,14 +1,59 @@
 package com.devopscat.mallapi.controller;
 
+import com.devopscat.mallapi.dto.PageRequestDTO;
+import com.devopscat.mallapi.dto.PageResponseDTO;
+import com.devopscat.mallapi.dto.TodoDTO;
+import com.devopscat.mallapi.service.TodoService;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+import java.util.function.ToDoubleFunction;
 
 @RestController
 @RequiredArgsConstructor
 @Log4j2
 @RequestMapping("/api/todo")
 public class TodoController {
+    // TodoService 주입
+    private final TodoService todoService;
+
+    // 특정 tno의 todo 조회
+    @GetMapping("/{tno}")
+    public TodoDTO get(@PathVariable Long tno) {
+        return todoService.get(tno);
+    }
+    // list/page=1&size=10 호출 처리
+    @GetMapping("/list")
+    public PageResponseDTO<TodoDTO> list(PageRequestDTO pageRequestDTO) {
+        log.info(pageRequestDTO);
+        return todoService.list(pageRequestDTO);
+    }
+
+    @PostMapping("/")
+    public Map<String, Long> register(@RequestBody TodoDTO todoDTO) {
+        log.info("TodoDTO: " + todoDTO);
+        Long tno = todoService.register(todoDTO);
+        return Map.of("TNO", tno);
+    }
+
+    @PutMapping("/{tno}")
+    public Map<String, String> modify(@PathVariable(name = "tno") Long tno, @RequestBody TodoDTO todoDTO) {
+        todoDTO.setTno(tno);
+
+        log.info("Modify: " + todoDTO);
+        todoService.modify(todoDTO);
+        return Map.of("RESULT", "SUCCESS");
+    }
+
+    @DeleteMapping("/{tno}")
+    public Map<String, String> remove(@PathVariable(name = "tno") Long tno) {
+
+        log.info("Remove: " + tno);
+        todoService.remove(tno);
+        return Map.of("RESULT", "SUCCESS");
+    }
 
 }
